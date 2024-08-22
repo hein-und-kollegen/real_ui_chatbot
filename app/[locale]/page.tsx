3dd9815840708cac879fdb164f2a2fc22aa8d850
Alex <alexander.reichel@hein-kollegen.de>
2024-08-22 16:16:25 +0200

    changincolors to hsl

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
       <div className="mt-2 text-4xl font-bold">Chatbot by Hein & Kollegen</div>
 
       <Link
-        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
+        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-slate-500 p-2 font-semibold"
         href="/login"
       >
         Start Chatting
