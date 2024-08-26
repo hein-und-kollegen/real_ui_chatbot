07b1418e5ed4738691f1d8257e49ebd861787c88
Alex <alexander.reichel@hein-kollegen.de>
2024-08-26 08:41:44 +0200

    chaning security

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -51,7 +51,7 @@ SECURITY DEFINER
 AS $$
 DECLARE
   project_url TEXT := 'https://fmcpfhlrmmuvkdspbpqa.supabase.co';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtY3BmaGxybW11dmtkc3BicHFhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDIyNTUxNCwiZXhwIjoyMDM5ODAxNTE0fQ.9ll-GZ3Bvsu0okhTG95cH3SJV_REZzymHQ8M4gIEGC4'; -- full access needed for http request to storage
+  service_role_key TEXT := getenv('SUPABASE_SERVICE_ROLE_KEY'); -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
