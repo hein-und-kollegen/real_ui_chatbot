6a6d58e08ee2c4da4e1177ba122b6a0626e2eca2
Alex <alexander.reichel@hein-kollegen.de>
2024-08-27 10:56:53 +0200

    button

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -13,10 +13,10 @@ export default function HomePage() {
       
       <img src="/images/H&K_Logo_45mm_weiß.svg" alt="Logo" width="150" height="auto" />
 
-      <div className="mt-4 text-4xl font-bold">Chatbot von Hein & Kollegen</div>
+      <div className="mt-8 text-4xl font-bold">Chatbot von Hein & Kollegen</div>
 
       <Link
-        className="mt-4 w-auto px-8 py-2 rounded-md bg-blue-950 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-blue-950 items-center justify-center"
+        className="mt-8 flex w-[200px] items-center justify-center rounded-md bg-slate-500 p-2 font-semibold"
         href="/login"
       >
         Chat starten
