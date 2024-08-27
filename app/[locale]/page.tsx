a4becadf61b35b677872f35b94af834394d54eec
Alex <alexander.reichel@hein-kollegen.de>
2024-08-27 10:25:14 +0200

    slate

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
       <div className="mt-2 text-4xl font-bold">Chatbot von Hein & Kollegen</div>
 
       <Link
-        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-slate-500 p-2 font-semibold"
+        className="hover:before:bg-redborder-slate-800 relative h-[50px] w-40 overflow-hidden border border-slate-800 bg-white px-3 text-grey-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full font-semibold"
         href="/login"
       >
         Chat starten
