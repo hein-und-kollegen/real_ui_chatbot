a96bc0be08e141b10d219fb6a5ad4bdf94abffef
Alex <alexander.reichel@hein-kollegen.de>
2024-08-27 09:27:50 +0200

    change-logo

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,15 +4,17 @@ import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
+import logo from '../public/images/H&K_Logo_weiß-01.png';
+import Image from 'next/image';
+
 
 export default function HomePage() {
   const { theme } = useTheme()
 
   return (
     <div className="flex size-full flex-col items-center justify-center">
-      <div>
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
-      </div>
+      
+      <Image src={logo} alt="Logo" width={300} height={300} />
 
       <div className="mt-2 text-4xl font-bold">Chatbot von Hein & Kollegen</div>
 
@@ -20,7 +22,7 @@ export default function HomePage() {
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-slate-500 p-2 font-semibold"
         href="/login"
       >
-        Starte den Chat
+        Chat starten
         <IconArrowRight className="ml-1" size={20} />
       </Link>
     </div>
